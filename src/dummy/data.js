e7028b4945974c700ae1b1b97856e21dd12f0154
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-04-05 09:00:08 +0545

    ch

---
+++ src/dummy/data.js
@@ -0,0 +1,55 @@
+
+
+export const mealData = {
+  "idMeal": "52772",
+  "strMeal": "Teriyaki Chicken Casserole",
+  "strDrinkAlternate": null,
+  "strCategory": "Chicken",
+  "strArea": "Japanese",
+  "strInstructions": "Preheat oven to 350° F. Spray a 9x13-inch baking pan with non-stick spray.\r\nCombine soy sauce, ½ cup water, brown sugar, ginger and garlic in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling.\r\nMeanwhile, stir together the corn starch and 2 tablespoons of water in a separate dish until smooth. Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat.\r\nPlace the chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove from oven and shred chicken in the dish using two forks.\r\n*Meanwhile, steam or cook the vegetables according to package directions.\r\nAdd the cooked vegetables and rice to the casserole dish with the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!",
+  "strMealThumb": "https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg",
+  "strTags": "Meat,Casserole",
+  "strYoutube": "https://www.youtube.com/watch?v=4aZr5hZXP_s",
+  "strIngredient1": "soy sauce",
+  "strIngredient2": "water",
+  "strIngredient3": "brown sugar",
+  "strIngredient4": "ground ginger",
+  "strIngredient5": "minced garlic",
+  "strIngredient6": "cornstarch",
+  "strIngredient7": "chicken breasts",
+  "strIngredient8": "stir-fry vegetables",
+  "strIngredient9": "brown rice",
+};
+
+
+export const medBlog = {
+  label: 'April Brings America\’s First Total Eclipse Since 2017: What You Need to Know',
+  author: {
+    name: 'Rebecca Jean T',
+    blogLength: '7 min read',
+    createdAt: 'Mar 8, 2024'
+  },
+  detail: 'In just one month the skies of North America, including Mexico, the United States, and Canada, will be graced with a total solar eclipse. This is the first total eclipse ',
+  liked: '3K',
+  comment: '44'
+};
+
+export const product = {
+  "id": 1,
+  "title": "iPhone 9",
+  "description": "An apple mobile which is nothing like apple",
+  "price": 549,
+  "discountPercentage": 12.96,
+  "rating": 4.69,
+  "stock": 94,
+  "brand": "Apple",
+  "category": "smartphones",
+  "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+  "images": [
+    "https://cdn.dummyjson.com/product-images/1/1.jpg",
+    "https://cdn.dummyjson.com/product-images/1/2.jpg",
+    "https://cdn.dummyjson.com/product-images/1/3.jpg",
+    "https://cdn.dummyjson.com/product-images/1/4.jpg",
+    "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+  ]
+};
